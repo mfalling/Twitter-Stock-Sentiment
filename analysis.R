11a2352db106cacb44d778a92790561233820643
mfalling <mfalling@usf.edu>
2021-03-13 02:25:01 -0500

    Collected Tweets

--- analysis.R
+++ analysis.R
@@ -1,15 +1,20 @@
 # Library -----------------------------------------------------------------
-
-
+library(rtweet)
 
 # About -------------------------------------------------------------------
 
 # First "real" repo using Github Desktop.
 
+# Collect & Subset Data ---------------------------------------------------
 
+# Collect up to 20k tweets containing GME, up until 6:59PM on Friday.
+rt <- search_tweets(q = "GME", 
+                    n = 20000, 
+                    max_id = 1370524864071696386,
+                    lang = "en")
 
-# Collect Data ------------------------------------------------------------
+write_as_csv(rt, "data/GME_03132021.csv")
 
-
+# Clean Data --------------------------------------------------------------
 
 
